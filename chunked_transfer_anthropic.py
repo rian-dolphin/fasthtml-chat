64ac65573344a7a3aaeef2b6de090fe1d71baf18
Rian <rdolphin98@gmail.com>
2024-08-20 15:27:52 +0100

    change to new htmx extension rather than script

--- chunked_transfer_anthropic.py
+++ chunked_transfer_anthropic.py
@@ -11,75 +11,9 @@ from starlette.responses import StreamingResponse
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
 
 # Set up the app, including daisyui and tailwind for the chat component
-htmx_extension_script = Script(
-    """
-(function(){
-  /** @type {import("../htmx").HtmxInternalApi} */
-  var api;
-  var lastResponseLength = 0;
-  var isComplete = false;
-
-  htmx.defineExtension('chunked-transfer', {
-      init: function(apiRef) {
-          api = apiRef;
-      },
-
-      onEvent: function(name, evt) {
-          if (name === "htmx:beforeRequest") {
-              var xhr = evt.detail.xhr;
-              var elt = evt.detail.elt;
-
-              isComplete = false;
-
-              xhr.onprogress = function() {
-                  if (isComplete) return;
-
-                  var isChunked = xhr.getResponseHeader("Transfer-Encoding") === "chunked" ||
-                                xhr.getResponseHeader("X-Transfer-Encoding") === "chunked";
-                  if (!isChunked) return;
-
-                  var response = xhr.response;
-                  var newChunk = response.substring(lastResponseLength);
-                  lastResponseLength = response.length;
-
-                  if (newChunk.trim() === "") return;
-
-                  api.withExtensions(elt, function(extension) {
-                      if (extension.transformResponse) {
-                          newChunk = extension.transformResponse(newChunk, xhr, elt);
-                      }
-                  });
-
-                  var swapSpec = api.getSwapSpecification(elt);
-                  var target = api.getTarget(elt);
-
-                  htmx.swap(target, newChunk, {
-                      swapStyle: swapSpec.swapStyle,
-                      swapDelay: swapSpec.swapDelay,
-                      settleDelay: swapSpec.settleDelay,
-                      ignoreTitle: true,
-                      appendOnly: true,
-                      contextElement: elt
-                  });
-              };
-
-              xhr.onload = function() {
-                  isComplete = true;
-              };
-
-              xhr.onloadend = function() {
-                  lastResponseLength = 0;
-                  isComplete = false;
-              };
-          }
-      }
-  });
-})();
-    """
-)
 hdrs = (
     picolink,
-    htmx_extension_script,
+    Script(src="https://unpkg.com/htmx-ext-transfer-encoding-chunked@0.3.0/transfer-encoding-chunked.js"),
     Script(src="https://cdn.tailwindcss.com"),
     Link(
         rel="stylesheet",
