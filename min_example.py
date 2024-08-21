1dca0dd147b02557ff0c70ed07d285f458cf1394
Rian <rdolphin98@gmail.com>
2024-08-21 11:28:12 +0100

    Add minimum example

---
+++ min_example.py
@@ -0,0 +1,20 @@
+from claudette import Client
+from fasthtml.common import *
+
+from fh_chat import Chat
+
+# os.environ["ANTHROPIC_API_KEY"] = "..."
+claudette_client = Client(model="claude-3-haiku-20240307")
+chat = Chat(claudette_client, sp="Respond with a haiku")
+
+app = FastHTML(cls="p-4 max-w-lg mx-auto")
+
+
+@app.get("/")
+def page():
+    return Chat.chat_page(include_style_headers=True)
+
+
+app.post("/generate-message")(chat.generate_message)
+
+serve()
